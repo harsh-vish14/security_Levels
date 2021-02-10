4ce02d9abd3c9509b1f5c88cd1a3c88dcf25d0c9
harsh-vish14 <harshkv9322@gmail.com>
2021-02-10 21:10:31 +0530

    Level 3 - security

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express')
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
@@ -16,8 +17,7 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
-const secret = 'Thisisourlittlesecret.'
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -39,6 +39,8 @@ app.route('/login')
                     if (data.password === password) {
                         res.render('secrets')
                     }
+                } else {
+                    console.log(data)
                 }
             }
         })
