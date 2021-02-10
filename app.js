b3adf21001b2f74a6e22c6acf0580ed34e134fbf
harsh-vish14 <harshkv9322@gmail.com>
2021-02-10 20:51:40 +0530

    Level 2 - Encryption security

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
-// const encrypt = require("mongoose-encryption")
+const encrypt = require("mongoose-encryption")
 
 const app = express()
 
@@ -16,8 +16,8 @@ const userSchema = new mongoose.Schema({
     password: String
 })
 
-// const secret = 'Thisisourlittlesecret.'
-// userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+const secret = 'Thisisourlittlesecret.'
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
